89e010e32caa92c85e12dc28c8f926c5007f21b7
joellawrence <lawrencej43@mymacewan.ca>
2023-04-26 21:19:27 -0600

    Create TodoList.js and Todo.js, and handleAddTodo functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList'
 
 function App() {
+    const [todos, setTodos] = useState([])
+    const todoNameRef = useRef()
+
+    function handleAddTodo(e) {
+        const name = todoNameRef.current.value
+        if (name === '') return
+        console.log(name)
+    }
+
     return (
-        <TodoList />
+        <>
+            <TodoList todoList={todos} />
+            <input ref={todoNameRef} type="text" />
+            <button onClick={handleAddTodo}>Add Todo</button>
+            <button>Clear Completed Todos</button>
+            <div>0 left to do</div>
+        </>
     )
 }
 
